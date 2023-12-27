59fcb946bcd344e9c8ee736f0a2d14f9d3489744
Jay Mingle <jkawudi@gmail.com>
2023-12-27 04:48:11 +0000

    Changes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Ghana')
+const calculateBMI = document.querySelector("#calculateBMI")
+
+const heightCM = document.querySelector("#the_height").value
+const weightKG = document.querySelector("#the_weight").value
+
+calculateBMI.onclick = () => {
+    alert("Calculate")
+    console.log("Height", heightCM)
+    console.log("Weight", weightKG)
+}
