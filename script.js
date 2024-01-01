c22919fe29eaa56342c551bf2cae0b76c11b41fe
Jay Mingle <jkawudi@gmail.com>
2024-01-01 13:20:35 +0000

    Changes

--- script.js
+++ script.js
@@ -12,21 +12,29 @@ function calculateData(){
     console.log("BMI", bmi_value)
 
     let msg = ''
+    let msg_status = ''
 
     if(bmi_value < 16){
-        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Severely Underweight</span>
+        msg = "Your BMI is " + bmi_value
+        msg_status = "Severely Underweight"
     }else if(bmi_value >= 16 && bmi_value <= 18.4){
-        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Underweight</span>
+        msg = "Your BMI is " + bmi_value
+        msg_status = "Underweight"
     }else if(bmi_value >= 18.5 && bmi_value <= 24.9){
-        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Normal</span>
+        msg = "Your BMI is " + bmi_value
+        msg_status = "Normal"
     }else if(bmi_value >= 25.0 && bmi_value <= 29.9){
-        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Overweight</span>
+        msg = "Your BMI is " + bmi_value
+        msg_status = "Overweight"
     }else if(bmi_value >= 30.0 && bmi_value <= 34.9){
-        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Moderately Obese</span>
+        msg = "Your BMI is " + bmi_value
+        msg_status = "Moderately Obese"
     }else if(bmi_value >= 35.0 && bmi_value <= 39.9){
-        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Severely Obese</span>
+        msg = "Your BMI is " + bmi_value
+        msg_status = "Severely Obese"
     }else{
-        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Morbidly Obese</span>
+        msg = "Your BMI is " + bmi_value
+        msg_status = "Morbidly Obese"
     }
     document.getElementById("report_msg").innerHTML += msg
 }
