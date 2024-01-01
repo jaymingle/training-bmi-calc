2b1729a094f4ca7e36907929630bb3676effdd71
Jay Mingle <jkawudi@gmail.com>
2024-01-01 05:26:26 +0000

    Changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const calculateBMI = document.querySelector("#calculateBMI")
 
-const heightCM = document.querySelector("#the_height").value
-const weightKG = document.querySelector("#the_weight").value
-
 calculateBMI.onclick = () => {
-    alert("Calculate")
+
+    const heightCM = document.querySelector("#the_height").value
+    const weightKG = document.querySelector("#the_weight").value
+
     console.log("Height", heightCM)
     console.log("Weight", weightKG)
 }
