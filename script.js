46735850a06f7c8ce80ee688eec6c2105d68fac9
Jay Mingle <jkawudi@gmail.com>
2024-01-01 13:08:24 +0000

    Changes

--- script.js
+++ script.js
@@ -14,7 +14,19 @@ function calculateData(){
     let msg = ''
 
     if(bmi_value < 16){
-        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status"></span>
+        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Severely Underweight</span>
+    }else if(bmi_value >= 16 && bmi_value <= 18.4){
+        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Underweight</span>
+    }else if(bmi_value >= 18.5 && bmi_value <= 24.9){
+        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Normal</span>
+    }else if(bmi_value >= 25.0 && bmi_value <= 29.9){
+        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Overweight</span>
+    }else if(bmi_value >= 30.0 && bmi_value <= 34.9){
+        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Moderately Obese</span>
+    }else if(bmi_value >= 35.0 && bmi_value <= 39.9){
+        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Severely Obese</span>
+    }else{
+        msg = "Your BMI is " + bmi_value + " " + <span id="msg_status">Morbidly Obese</span>
     }
 
 }
