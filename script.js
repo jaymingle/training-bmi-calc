16af17100e40c8cfee6a4ede48d7ffe9d5fa74b2
Jay Mingle <jkawudi@gmail.com>
2024-01-01 12:20:03 +0000

    Changes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const height = document.querySelector("#the_height").value
-const weight = document.querySelector("#the_weight").value
 
 function calculateData(){
+    let height = document.querySelector("#the_height").value
+    let weight = document.querySelector("#the_weight").value
+
     console.log("Calculating")
     console.log(height)
     console.log(weight)
