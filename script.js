e29e88a4dec7def1e6b2f8a15c89c41dd89eb190
Jay Mingle <jkawudi@gmail.com>
2024-01-01 12:54:09 +0000

    Changes

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ function calculateData(){
     let height = Number(document.querySelector("#the_height").value)
     let weight = Number(document.querySelector("#the_weight").value)
 
-    let bmi_value = weight/Math.pow(height,2) * 10000
+    let bmi_value = Number((weight/Math.pow(height,2) * 10000).toFixed(2))
 
     console.log("Calculating")
     console.log(height)
     console.log(weight)
     console.log("BMI", bmi_value)
+
+    let msg = ''
+
+    if(bmi_value < 16){
+        msg = "Your BMI is " + bmi_value + " " + <span id=""></span>
+    }
+
 }
 
 function resetData(){
