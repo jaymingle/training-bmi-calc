094528e9b888b7d0530b2f4f5308822ca4fcc64d
Jay Mingle <jkawudi@gmail.com>
2024-01-01 12:34:19 +0000

    Changes

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ function calculateData(){
     let height = Number(document.querySelector("#the_height").value)
     let weight = Number(document.querySelector("#the_weight").value)
 
+    let bmi_value = weight/Math.pow(height,2) * 10000
+
     console.log("Calculating")
     console.log(height)
     console.log(weight)
+    console.log("BMI", bmi_value)
 }
 
 function resetData(){
